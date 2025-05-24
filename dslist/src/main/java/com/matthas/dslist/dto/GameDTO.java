855317b27c7ade763b111acf6686813da99488bd
unknown <asaphmateus01@gmail.com>
2025-05-24 16:18:18 -0300

    Get game by id

--- dslist/src/main/java/com/matthas/dslist/dto/GameDTO.java
+++ dslist/src/main/java/com/matthas/dslist/dto/GameDTO.java
@@ -1,53 +1,27 @@
-package com.matthas.dslist.entities;
+package com.matthas.dslist.dto;
 
-import java.util.Objects;
+import org.springframework.beans.BeanUtils;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.matthas.dslist.entities.Game;
 
-@Entity
-@Table(name = "tb_game")
-public class Game {
+public class GameDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-
-    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
-
-    @Column(columnDefinition = "Text")
     private String shortDescription;
-
-    @Column(columnDefinition = "Text")
     private String longDescription;
 
-
-    public Game() {
+    public GameDTO() {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
-                String shortDescription, String longDescription) {
-
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.platforms = platforms;
-        this.score = score;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
+    public GameDTO(Game entity) {
+        BeanUtils.copyProperties(entity, this);
     }
 
     public Long getId() {
@@ -86,7 +60,7 @@ public class Game {
         return platforms;
     }
 
-    public void setPlatform(String platforms) {
+    public void setPlatforms(String platforms) {
         this.platforms = platforms;
     }
 
@@ -122,15 +96,5 @@ public class Game {
         this.longDescription = longDescription;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Game game = (Game) o;
-        return Objects.equals(id, game.id);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
--- dslist/src/main/java/com/matthas/dslist/services/GameService.java
+++ dslist/src/main/java/com/matthas/dslist/services/GameService.java
@@ -1,12 +1,15 @@
 package com.matthas.dslist.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.matthas.dslist.dto.GameDTO;
 import com.matthas.dslist.dto.GameMinDTO;
 import com.matthas.dslist.entities.Game;
 import com.matthas.dslist.repositories.GameRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class GameService {
@@ -14,6 +17,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional
+    public GameDTO findByID(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+        
+    }
+
+    @Transactional
     public List<GameMinDTO> findAll() {
        List<Game> result = gameRepository.findAll();
        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
